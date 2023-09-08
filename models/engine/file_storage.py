70f7c33f3836d949be7ec1fde9bd227b8b2006d9
eldadgishinge <e.gishinge@alustudent.com>
2023-09-08 14:56:09 +0200

    alu-AirBnB

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -43,14 +43,6 @@ class FileStorage:
         """
         deserializes the JSON file to __objects (only if the JSON file
         """
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.state import State
-        from models.city import City
-        from models.place import Place
-        from models.amenity import Amenity
-        from models.review import Review
-
         try:
             with open(self.__file_path) as file:
                 serialized_content = json.load(file)
