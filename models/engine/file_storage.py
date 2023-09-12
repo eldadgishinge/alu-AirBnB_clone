a025f3c71225494fdb51be09c58433e882c45f0c
eldadgishinge <e.gishinge@alustudent.com>
2023-09-12 12:47:45 +0200

    alu-AirBnB_clone

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,12 +1,23 @@
+#!/usr/bin/python3
+"""Defines FileStorage class."""
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+
 
 class FileStorage:
     """
     Class FileStorage
-    Represents an abstracted storage engine.
+    Represent an abstracted storage test_engine.
 
     It serializes instances to a JSON file and deserializes
-    JSON files to instances.
+    JSON file to instances.
+
     """
     __file_path = 'file.json'
     __objects = {}
@@ -16,44 +27,35 @@ class FileStorage:
         return self.__objects
 
     def new(self, obj):
-        """Set in __objects obj with the key <obj_class_name>.id"""
+        """Set in __objects obj with the  key <obj_class_name>.id"""
         key = '{}.{}'.format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
         """Serialize __objects to JSON file __file_path."""
         object_dict = {}
-        for key, obj in self.__objects.items():
-            object_dict[key] = obj.to_dict()
+        for obj in self.__objects:
+            object_dict[obj] = self.__objects[obj].to_dict()
         with open(self.__file_path, 'w') as file:
             json.dump(object_dict, file)
 
     def reload(self):
         """
-        Deserializes the JSON file to __objects (only if the JSON file exists).
+        deserializes the JSON file to __objects (only if the JSON file
         """
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.place import Place
+        from models.amenity import Amenity
+        from models.review import Review
+
         try:
             with open(self.__file_path) as file:
                 serialized_content = json.load(file)
-                for key, value in serialized_content.items():
-                    class_name = value['__class__']
-                    self.new(eval(class_name)(**value))
+                for item in serialized_content.values():
+                    class_name = item['__class__']
+                    self.new(eval(class_name + "(**" + str(item) + ")"))
         except FileNotFoundError:
             pass
-
-    def delete(self, obj=None):
-        """
-        Deletes obj from __objects if it's present.
-        """
-        if obj:
-            key = '{}.{}'.format(obj.__class__.__name__, obj.id)
-            if key in self.__objects:
-                del self.__objects[key]
-
-    def close(self):
-        """
-        Calls reload() method to deserialize objects
-        and close the session.
-        """
-        self.reload()
-
